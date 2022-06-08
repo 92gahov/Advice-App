c3aeb7dbf35485c96665d0465ee312122a731061
gahov <georgi.gahov@gmail.com>
2022-06-09 00:01:01 +0300

    Advice App

--- src/Components/Advice.js
+++ src/Components/Advice.js
@@ -1,28 +1,42 @@
 import { Component } from "react";
+import Loader from "./Loader";
 
 class Advice extends Component {
     constructor() {
         super();
         this.state = {
-            advice: ""
+            advice: "",
+            loading: false
         }
     }
-    getAdvice = () => {
+    fetchAdvice = () => {
+        this.setState({
+            loading: true
+        })
         fetch("https://api.adviceslip.com/advice")
             .then((response) => response.json())
             .then((json) => {
                 this.setState({
-                    advice: json.slip.advice
+                    advice: json.slip.advice,
+                    loading: false
                 })
             })
     }
+
+    getAdvice() {
+        this.fetchAdvice();
+    }
+
     render() {
-        const { advice } = this.state
+        const { advice, loading } = this.state
         return (
             <div>
                 <h1>Random Advice App</h1>
                 <button onClick={() => this.getAdvice()}>Click</button>
-                <p>{advice}</p>
+                {/* <p>{advice}</p> */}
+                <div>
+                    {!loading ? <p>{advice}</p> : <Loader />}
+                </div>
             </div>
         )
     }
